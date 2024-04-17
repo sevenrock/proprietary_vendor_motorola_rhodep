9b7c5893cb02dba53c544bc30dc0ac3613defedd
Julian Veit <Claymore1298@gmail.com>
2024-04-17 19:05:59 +0200

    rhodep: add libmotoisdataqueue.so
    04-17 09:04:01.226  4591  4591 E vndksupport: Could not load /vendor/lib64/hw/camera.qcom.so from default namespace: dlopen failed: library "libmotoisdataqueue.so" not found: needed by /vendor/lib64/hw/camera.qcom.so in namespace (default).
    04-17 09:04:01.226  4591  4591 E HAL     : load: module=/vendor/lib64/hw/camera.qcom.so
    04-17 09:04:01.226  4591  4591 E HAL     : unknown
    04-17 09:04:01.226  4591  4591 E CamPrvdr@2.4-legacy: Could not load camera HAL module: -22 (Invalid argument)
    04-17 09:04:01.226  4591  4591 E android.hardware.camera.provider@2.4-service_64: getProviderImpl: camera provider init failed!
    
    Binary taken from rhodep dump T1SUS33.1-124-6-7.
    
    Signed-off-by: Sevenrock <sevenrock@hotmail.de>

--- rhodep-vendor.mk
+++ rhodep-vendor.mk
@@ -329,6 +329,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/rhodep/proprietary/vendor/lib64/libmmcamera_pdpc.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmmcamera_pdpc.so \
     vendor/motorola/rhodep/proprietary/vendor/lib64/libmot_afd.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmot_afd.so \
     vendor/motorola/rhodep/proprietary/vendor/lib64/libmotohid.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmotohid.so \
+    vendor/motorola/rhodep/proprietary/vendor/lib64/libmotoisdataqueue.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmotoisdataqueue.so \
     vendor/motorola/rhodep/proprietary/vendor/lib64/libmpbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmpbase.so \
     vendor/motorola/rhodep/proprietary/vendor/lib64/libofflinedump.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libofflinedump.so \
     vendor/motorola/rhodep/proprietary/vendor/lib64/libofflinelog.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libofflinelog.so \
