8c1efc4e898344452e6d7851b733745914e9b44b
Sevenrock <sevenrock@hotmail.de>
2024-12-12 23:35:39 +0100

    rhodep: Import device firmware

--- Android.mk
+++ Android.mk
@@ -3,3 +3,24 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),rhodep)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,dbf1f48aa333588eae3f3e93d22161c19e451f3e)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,cade701c973412e1db93486b2ba1d9b1e302ed80)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,1fc0c54b63d571fd8cdc867c6cdf20f4d79511d6)
+$(call add-radio-file-sha1-checked,radio/dsp.img,82264c7af1f4d41dfe9c4783c2880a75a4801bb1)
+$(call add-radio-file-sha1-checked,radio/fsg.img,0713fefe2bf6e885c11864c624626c7c0f1eb415)
+$(call add-radio-file-sha1-checked,radio/hyp.img,df57a21c711ba7c6d7c9e83baf623d434c9bb8ed)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,e572ece9c84d457e5f1facfe966da8bfb8ccba3b)
+$(call add-radio-file-sha1-checked,radio/logo.img,021d5f46f92ea3124a9fbd681b32c0b0541f8f55)
+$(call add-radio-file-sha1-checked,radio/modem.img,4d238a9649d1edc881d9ca7b4ee866c33b35d3cc)
+$(call add-radio-file-sha1-checked,radio/prov.img,bb7f833dd709e89dc737041c14fb1e0f3169330b)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,eb064c33ebcd25912e4d2bc67286951c1f1eca7f)
+$(call add-radio-file-sha1-checked,radio/storsec.img,264f8fed7a7cca93fc7cb1a37dc4548e5940f34f)
+$(call add-radio-file-sha1-checked,radio/tz.img,e392c1b4185eb36242b9720dab6ba2fe3264f4d3)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,13d8f9a2d9282634a23247f1db4b9add834a3f67)
+$(call add-radio-file-sha1-checked,radio/xbl.img,57556beca97ce48faa6a5ddc6c62ccf263b99c05)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,b1d1f2920f2d263d0fb4cc4a4b1db2a2d58fa047)
+
+endif
--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,3 +1,21 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
+
+AB_OTA_PARTITIONS += \
+    abl \
+    bluetooth \
+    devcfg \
+    dsp \
+    fsg \
+    hyp \
+    keymaster \
+    logo \
+    modem \
+    prov \
+    qupfw \
+    storsec \
+    tz \
+    uefisecapp \
+    xbl \
+    xbl_config
